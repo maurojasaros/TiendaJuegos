69a1a9be136316ea2f0095d67d57f96e702a5813
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2024-09-28 14:53:41 -0300

    no pude seguir con los token, asique solo estoy modificando los .html

--- TiendaJuegos/urls.py
+++ TiendaJuegos/urls.py
@@ -17,17 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('juegos.urls')),
     path('', lambda request: redirect('juegos/')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    
 ]
